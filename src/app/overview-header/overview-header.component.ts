36ecea9e9d506782f8eed85a9bfa5d9f2fb8faed
Oskar Ek <oskarek24@gmail.com>
2017-03-22 10:23:59 +0100

    fixed overview-header component

--- src/app/overview-header/overview-header.component.ts
+++ src/app/overview-header/overview-header.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MyDinnerComponent } from '../my-dinner/my-dinner.component';
+import { DishService } from '../dish.service';
 
 @Component({
   selector: 'overview-header',
   templateUrl: './overview-header.component.html',
   styleUrls: ['./overview-header.component.css']
 })
-export class OverviewHeaderComponent implements OnInit {
+export class OverviewHeaderComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(public dishService: DishService) { }
 
 }
