1a1377e06a30646565ef1574620f043ad22ee46f
Oskar Ek <oskarek24@gmail.com>
2017-03-22 09:44:52 +0100

    preparation view done

--- src/app/preparation/preparation.component.ts
+++ src/app/preparation/preparation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Dish } from '../model/dish';
 import { DishService } from '../dish.service';
 
@@ -7,20 +7,8 @@ import { DishService } from '../dish.service';
   templateUrl: './preparation.component.html',
   styleUrls: ['./preparation.component.css']
 })
-export class PreparationComponent implements OnInit {
+export class PreparationComponent {
 
   constructor(public dishService: DishService) { }
-
-  ngOnInit() {
-    var dish = new Dish();
-    dish.id = 0;
-    dish.name = "Hamburgare";
-    this.dishService.addDishToMenu(dish);
-
-    var dish2 = new Dish();
-    dish2.id = 1;
-    dish2.name = "Kaka";
-    this.dishService.addDishToMenu(dish2);
-  }
 
 }
